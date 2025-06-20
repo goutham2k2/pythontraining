7b66dad36a064416b9745e6bf63e6a6241f56003
goutham k <kalamalla34.goutham@gmail.com>
2025-06-20 21:47:32 +0530

    listPro

--- basic/datatypes/ListPro/ListPro.py
+++ basic/datatypes/ListPro/ListPro.py
@@ -11,13 +11,6 @@ list2 = [1, 2, 3, 4, 5, 6, 7 ]
 print ("list1[0]: ", list1[0])
 print ("list2[1:5]: ", list2[1:5])
 
-"""
-list1[0]:  physics
-list2[1:5]:  [2, 3, 4, 5]
-=======
-"""
-
-"""
 
 #1. Find the Sum of All Elements in a List
 
