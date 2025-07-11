57d7a115d3e32a75c0c33f8586b15fdc26108bed
NarayanaGolla <narayana.golla.aws@gmail.com>
2025-07-11 08:31:20 +0530

    MRO

--- advance/Files/FileOperations.py
+++ advance/Files/FileOperations.py
@@ -1,2 +1,6 @@
-print("navya" , end='')
-print("navya")
+#print("navya" , end='')
+#print("navya")
+
+with open("example_with_content.txt", "a") as file:
+    file.write("Hello, this is a new file with some content!")
+print("File with content created successfully.")
--- advance/pytestsample/basic1/file_utils.py
+++
@@ -1,9 +0,0 @@
-# file_utils.py
-
-def write_to_file(path, content):
-    with open(path, 'w') as f:
-        f.write(content)
-
-def read_from_file(path):
-    with open(path, 'r') as f:
-        return f.read()
